7f54e0272c8826cb090af8b06b6e6b62dda2b5ca
Rodger Byrd <rodger.byrd@gmail.com>
2018-09-18 12:55:17 -0700

    initial project setup

---
+++ src/Project1.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Project Name:		Project1.cpp
+// Build instructions:	g++ Project1.cpp -o project1
+// Name: 				Rodger Byrd
+// Function: 			Read in Machine files consecutively
+//						Read in strings
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+
+
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
